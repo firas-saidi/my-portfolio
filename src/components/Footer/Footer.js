f0c38cda80fe470533054e64e7765844b74b0082
unknown <saidi.firas@outlook.com>
2022-11-22 21:34:08 +0100

    Fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className="last-text">
-    <p>Developed with love by Saidi Firas © 2023
+    <p>Developed with love by Saidi Firas © 2022
     <a href="#home" className="top"><i className='bx bx-up-arrow-alt' ></i></a>
     </p>
     </div>
